df889584f080be7e40a37a282cb6019389d61072
Behtmoth-Jr <79050532+Behtmoth-Jr@users.noreply.github.com>
2024-03-19 03:12:10 +0300

    12

--- js/app.js
+++ js/app.js
@@ -7,45 +7,7 @@ window.addEventListener('load', async () => {
       console.log('Service worker register fail')
     }
   }
-
-  await loadEmojis()
 })
-
-function loadEmojis() {
-  fetch('https://api.github.com/emojis')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(data => {
-      const emojisContainer = document.getElementById('emojis-container');
-      Object.keys(data).forEach(key => {
-        const img = document.createElement('img');
-        img.src = data[key];
-        img.alt = key;
-
-        // Создание контейнера для каждого эмодзи и его названия
-        const emojiContainer = document.createElement('div');
-        emojiContainer.classList.add('emoji-wrapper');
-
-        // Создание элемента для вывода названия
-        const emojiName = document.createElement('p');
-        emojiName.textContent = key; // Установка текста названия
-
-        // Добавление эмодзи и названия в контейнер
-        emojiContainer.appendChild(img);
-        emojiContainer.appendChild(emojiName);
-
-        // Добавление каждого контейнера на страницу
-        emojisContainer.appendChild(emojiContainer);
-      });
-    })
-    .catch(error => {
-      console.error('There was a problem with the fetch operation:', error);
-    });
-}
 
 let deferredPrompt; // Переменная для хранения события beforeinstallprompt
 
@@ -71,5 +33,3 @@ let deferredPrompt; // Переменная для хранения событи
         deferredPrompt = null;
       });
     });
-
-document.addEventListener("DOMContentLoaded", loadEmojis);
